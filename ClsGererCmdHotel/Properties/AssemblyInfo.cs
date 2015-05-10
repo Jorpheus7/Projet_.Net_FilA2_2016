b5bdfee48980b91593382c8bb292bf55ab1f24e5
Thibault <thibault.devoye@hotmail.fr>
2015-05-10 18:16:05 +0200

    Config pour MSMQ

--- ClsGererCmdHotel/Properties/AssemblyInfo.cs
+++ ClsGererCmdHotel/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.EnterpriseServices;
 
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
@@ -17,7 +18,9 @@ using System.Runtime.InteropServices;
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
+[assembly: ApplicationActivation(ActivationOption.Server)]
+
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("61c49c39-edfb-4834-b02c-f3de0c1b726f")]
