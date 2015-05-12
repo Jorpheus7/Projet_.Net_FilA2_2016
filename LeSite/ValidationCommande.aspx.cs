d829c04f5ba6f97d9ebf84e7f947f2f3debc26aa
Roman Lapacherie <roman.lapacherie@gmail.com>
2015-05-12 11:34:58 +0100

    Ajout des méthodes pour reserver un vol ou un hotel

--- LeSite/ValidationCommande.aspx.cs
+++ LeSite/ValidationCommande.aspx.cs
@@ -20,11 +20,22 @@ public partial class ValidationCommande : System.Web.UI.Page
 
         if (idVol != null)
         {
-            //Recuperer le vol selon l'id et l'afficher
+            WSVol myWSVol = new WSVol();
+            vol = myWSVol.vol_id(int.Parse(idVol));
         }
         if (idHotel != null)
         {
-            //Recuperer l'hotel selon l'id et l'afficher
+            WSHotel myWSHotel = new WSHotel();
+            hotel = myWSHotel.hotel_id(int.Parse(idHotel));
+            Chambre[] chambres = myWSHotel.liste_chambres_hotels(idHotel);
+
+            foreach (var chambre in chambres) {
+                ListItem item = new ListItem();
+                item.Value = chambre.id.ToString();
+                item.Text = "Chambre N°" + chambre.numero + " au prix de : " + chambre.prix + "€";
+                CheckboxChambres.Items.Add(item);
+            }
+
         }
 
     }
@@ -39,8 +50,29 @@ public partial class ValidationCommande : System.Web.UI.Page
         String pays = paysInscription.Text;
         ClsGererCmdHotel myHotel = new ClsGererCmdHotel();
         ClsGererCmdVol myVol = new ClsGererCmdVol();
-        myHotel.new_client(nom, prenom, adresse, cp, ville, tel, pays);
-        myVol.new_client(nom, prenom, adresse, cp, ville, tel, pays);
+        voyage.cmdHotel.Client clientHotel = myHotel.new_client(nom, prenom, adresse, cp, ville, tel, pays);
+        voyage.cmdVol.Client clientVol = myVol.new_client(nom, prenom, adresse, cp, ville, tel, pays);
+
+        if (this.vol != null)
+        {
+            int nbPersonne = int.Parse(TextNbPersonneVol.Text);
+            myVol.new_cmdvol_existing_client(vol.id, clientVol.id, DateTime.Now.ToString("dd/MM/yyyy"), nbPersonne, vol.prix);
+        }
+        if (this.hotel != null)
+        {
+            WSHotel myWSHotel = new WSHotel();
+            Chambre[] chambres = myWSHotel.liste_chambres_hotels(hotel.id.ToString());
+            Chambre chambre = null;
+            foreach (var c in chambres){
+                if(c.id == int.Parse(CheckboxChambres.SelectedItem.Value)){
+                    chambre = c;
+                }
+            }
+
+            int nbPersonne = int.Parse(TextNbPersonneHotel.Text);
+            myHotel.new_cmdhotel_existing_client(chambre.id, clientVol.id, DateTime.Now.ToString("dd/MM/yyyy"), nbPersonne, chambre.prix,
+                dateDepart.Text, dateRetour.Text);
+        }
     }
     protected void btnConnexion_Click(object sender, EventArgs e)
     {
